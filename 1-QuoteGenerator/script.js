a7088e752b87e47fe46a4654bbebca6762060dc4
Haticesurumlu <hatice.surumlu@ogr.sakarya.edu.tr>
2023-01-23 20:24:21 +0300

    feat: add the variables

--- 1-QuoteGenerator/script.js
+++ 1-QuoteGenerator/script.js
@@ -1,6 +1,12 @@
 
+const quoteContainer=document.getElementById('quote-container');
+const quoteText=document.getElementById('quote');
+const authorText=document.getElementById('author');
+const twitterBtn=document.getElementById('twitter');
+const newQuoteBtn=document.getElementById('new-quote');
 
-//let apiQuotes= [];
+
+let apiQuotes= [];
 
 //Show New Quote
 function newQuote() {
